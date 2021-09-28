fixed variance bug

evolutionary_strategies.py
@@ -31,14 +31,10 @@ class Evolutionary_Strategies:
     def calculate1fifth(self):
        
         # get the elements of the queue to a list
-        children = list(self.q.queue)
-
-        # generate the list of children scores
-        children_scores = [self.eval_func(child) for child in children]
-        # print(children_scores)
+        children_scores = list(self.q.queue)
 
         # convert to numpy array
-        children_scores_np = np.asarray(children_scores)
+        children_scores_np = np.asarray(list(self.q.queue))
 
         # get the number of successful children
         num_successful_children = (children_scores_np > self.best_so_far).sum()
@@ -46,7 +42,7 @@ class Evolutionary_Strategies:
         # print("Length of children ",len(children))
 
         # divide the number of successful children by total children
-        success_rate = num_successful_children / len(children)
+        success_rate = num_successful_children / len(children_scores)
         print("Success rate: ", success_rate)
 
         return success_rate
@@ -78,7 +74,6 @@ class Evolutionary_Strategies:
                 
                 child = parent.copy()
                 modifier = np.random.normal(loc=0.0, scale=self.sigma)
-                print("sigma: ", self.sigma)
                 # print(self.best_so_far)
                 # print("Modifier: ", modifier)
 
@@ -104,17 +99,22 @@ class Evolutionary_Strategies:
                     # Removing element from queue
                     # print("\nElement dequeued from the queue")
                     self.q.get()
-                self.q.put(child)
+                self.q.put(self.eval_func(child))
                 # print(self.q.qsize())
 
                 mutations_so_far += 1
 
-                if(mutations_so_far%self.n == 0 and self.q.qsize()==10*self.n ):
+                if(mutations_so_far%self.n == 0 and self.q.full()):
                     success = self.calculate1fifth()
                     if success > 0.2:
-                        self.sigma = self.sigma/0.85
+                        if self.sigma/0.85 <= 32:
+                            self.sigma = self.sigma/0.85
                     elif success < 0.2:
                         self.sigma = 0.85*self.sigma
+                    # if self.sigma > 32:
+                    #     lower, upper = 0, 32
+                    #     self.sigma =  lower + (upper - lower) * self.sigma
+                    print("New sigma: ", self.sigma) # Does the new sigma need to be withing bounds?
                     mutations_so_far=0
 
             return children
@@ -124,9 +124,6 @@ class Evolutionary_Strategies:
         Sorts the list based on the evaluation function and returns the first m elements.
         """
         m_best = sorted(pop, key=lambda child: self.eval_func(child))[:self.m]
-        child_score = self.eval_func(m_best[0])
-        if child_score < self.best_so_far:
-            self.best_so_far = child_score
         return m_best
 
     def es(self, m=5, l=30, g_max=5, eval_f= None, seed=0, initial_sigma=0.05, recombination_f=None, strategy="k+l", n=2):
@@ -177,6 +174,12 @@ class Evolutionary_Strategies:
                 if(strategy == "k,l"):
                     pop = []
                 for parent in parents:
+
+                    # check if this parent has the best score so far
+                    parent_score = self.eval_func(parent)
+                    if parent_score < self.best_so_far:
+                        self.best_so_far = parent_score
+
                     children = self.mutation_same_std_dev(parent)
                     # print("Children from 1 parent: ", children)
                     # print("l/m: ", l//m)